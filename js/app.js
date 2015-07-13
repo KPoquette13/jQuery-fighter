aeb0bbae0e6cc518e6c7346c21adbd559f2d2dd7
Kurt Poquette <kxp1607@rit.edu>
2015-07-13 17:36:27 -0500

    Hadouken working

---
+++ js/app.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function(){
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function(){
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},		//value, desired end value
+			500, 					//time taken
+			function(){				//callback for after finished
+				$(this).hide();
+				$(this).css('left', '580px');
+			}
+		);
+	})
+	.mouseup(function(){
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	})
+});
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
